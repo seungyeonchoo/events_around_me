aa8f4eaa319fbb12b046a66336638d0bc07b2042
cos4338@gmail.com <cos4338@gmail.com>
2023-05-17 22:34:56 +0100

    feat: implement TitleWithDescription component(MOLECULES/TitleWithDescription)

---
+++ src/components/Molecules/TitleWithDescription/index.tsx
@@ -0,0 +1,30 @@
+import Text from '../../Atoms/Text';
+
+export interface TitleWithDescriptionProps {
+  direction: 'col' | 'row';
+  title_text: string;
+  description: string;
+  title_color: 'primary' | 'default' | 'secondary' | 'dark';
+  text_color: 'primary' | 'default' | 'secondary' | 'dark';
+}
+
+const TitleWithDescription = ({
+  title_text,
+  title_color,
+  text_color,
+  description,
+  ...rest
+}: TitleWithDescriptionProps) => {
+  return (
+    <section className={`m-auto flex flex-col items-center justify-between`}>
+      <Text text_tag="h1" size="4xl" color={title_color} bold extra_style="my-8">
+        {title_text}
+      </Text>
+      <Text text_tag="p" size="sm" color={text_color}>
+        {description}
+      </Text>
+    </section>
+  );
+};
+
+export default TitleWithDescription;
