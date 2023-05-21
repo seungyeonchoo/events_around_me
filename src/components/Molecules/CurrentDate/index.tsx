db707dfc59ea76b18fc7482956cc5d9eca8f8734
cos4338@gmail.com <cos4338@gmail.com>
2023-05-21 19:39:11 +0100

    feat: implement date convert util function (getConvertedDate)

--- src/components/Molecules/CurrentDate/index.tsx
+++ src/components/Molecules/CurrentDate/index.tsx
@@ -1,18 +1,13 @@
+import { getConvertedDate } from '../../../lib/dateUtils';
 import Text from '../../Atoms/Text';
 
 export interface CurrentDateProps {}
 
-const MONTH = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
-const DAY = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-
-const CurrentDate = (props: CurrentDateProps) => {
-  const currentYear = new Date().getFullYear();
-  const currentMonth = MONTH[new Date().getMonth()];
-  const currentDate = new Date().getDate();
-  const currentDay = new Date().getDay();
+const CurrentDate = ({ ...rest }: CurrentDateProps) => {
+  const curr = new Date().toDateString();
   return (
     <Text size="2xl" text_tag="p" color="secondary" extra_style="text-center mt-10" bold>
-      {`${currentDate}. ${currentMonth}. ${currentYear} (${DAY[currentDay]})`}
+      {getConvertedDate(curr, true)}
     </Text>
   );
 };
