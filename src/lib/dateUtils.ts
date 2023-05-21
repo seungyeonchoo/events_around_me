857111166afabc7a581bb4400827aaea09531f09
cos4338@gmail.com <cos4338@gmail.com>
2023-05-21 23:27:01 +0100

    fix: fix getRamainDate function to include end date

--- src/lib/dateUtils.ts
+++ src/lib/dateUtils.ts
@@ -4,7 +4,7 @@ const DAY = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 export const getRemainDate = (date: string) => {
   const curr = new Date().getTime();
   const end = new Date(date).getTime();
-  const remain = Math.floor((end - curr) / (1000 * 60 * 60 * 24));
+  const remain = Math.ceil((end - curr) / (1000 * 60 * 60 * 24));
 
   return remain === 0 ? 'DUE' : remain < 0 ? 'END' : remain;
 };
