3c89b5d2682627434c95ddba0db25abf0f24bba8
cos4338@gmail.com <cos4338@gmail.com>
2023-05-22 23:15:27 +0100

    style: apply responsive layout design to HabitDetail/BasicDetail component

--- src/components/habit/HabitDetail/components/BasicDetail/index.tsx
+++ src/components/habit/HabitDetail/components/BasicDetail/index.tsx
@@ -7,14 +7,26 @@ export interface BasicDetailProps {
 
 const BasicDetail = ({ habit }: BasicDetailProps) => {
   return (
-    <section className="m-auto w-[40rem] h-[15rem] py-5 flex flex-col items-center">
-      <Text color="primary" size="4xl" text_tag="h1" bold>
+    <section className="m-auto h-[15rem] py-5 flex flex-col items-center w-[40rem] max-sm:w-full">
+      <Text
+        color="default"
+        size="3xl"
+        text_tag="h1"
+        bold
+        extra_style="p-2 bg-primary max-sm:text-[1rem] w-full text-center"
+      >
         {habit?.title}
       </Text>
-      <Text color="dark" size="xl" text_tag="p" bold>
+      <Text color="dark" size="xl" text_tag="p" bold extra_style="max-sm:text-[0.8rem]">
         {habit?.start_date} ~ {habit?.end_date}
       </Text>
-      <Text color="secondary" size="lg" text_tag="p" extra_style="my-10" italic>
+      <Text
+        color="secondary"
+        size="lg"
+        text_tag="p"
+        extra_style="my-10 max-sm:my-3 max-sm:text-[0.8rem]"
+        italic
+      >
         {habit?.description}
       </Text>
     </section>
